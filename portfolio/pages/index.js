3d866a417865632b44556c828a291a20b6eda7f0
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-05-02 09:43:24 +0800

    Init & Set up

---
+++ portfolio/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Aaron Chen</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
