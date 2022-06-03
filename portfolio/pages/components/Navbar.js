22b5ed6f318d198ec00cf7f4e15cec0cce019cec
Aaron Chen <aaron880221@gmail.com>
2022-06-04 01:48:35 +0800

    Responsive Navbar

--- portfolio/pages/components/Navbar.js
+++ portfolio/pages/components/Navbar.js
@@ -1,37 +1,101 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 function Navbar() {
+    const [open, setOpen] = useState("");
+    const [visibility, setVisibility] = useState("hidden");
+    const [isClick, setIsClick] = useState(false);
+    const handleIsClick = () => {
+        setIsClick((isClick) => !isClick);
+    };
+
+    useEffect(() => {
+        if (isClick) {
+            setVisibility("flex");
+            setOpen("open");
+        } else {
+            setVisibility("hidden");
+            setOpen("");
+        }
+    }, [isClick]);
+
     return (
-        <nav className="relative container mx-auto p-6">
+        // Navigation Bar
+        <nav className="relative w-screen p-6">
+            {/* Desktop Menu */}
             <div className="flex items-center justify-between">
-                <div className="flex items-center pt-2">
+                <div className="flex items-center">
                     <Link href="/">
                         <a>
-                            <Image src="/logo.svg" alt="Logo" width="60vh" height="60vw" />
+                            <Image src="/logo.svg" alt="Logo" height="60vh" width="60vw" />
                         </a>
                     </Link>
                     <Link href="#">
-                        <a className="max-w-md m-4 text-4xl text-bold font-bold hover:text-lightYellow">Aaron Chen</a>
+                        <a className="m-4 text-2xl text-bold font-bold lg:text-4xl">Aaron Chen</a>
                     </Link>
                 </div>
 
-                <div className="hidden space-x-6 md:flex">
+                {/* Menu Items */}
+                <div className="hidden items-center md:flex">
                     <Link href="#">
-                        <a className="hover:text-lightYellow">Home</a>
+                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Home</a>
                     </Link>
                     <Link href="#">
-                        <a className="hover:text-lightYellow">About</a>
+                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">About</a>
                     </Link>
                     <Link href="#">
-                        <a className="hover:text-lightYellow">Projects</a>
+                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Projects</a>
                     </Link>
                     <Link href="#">
-                        <a className="hover:text-lightYellow">Contact</a>
+                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Contact</a>
                     </Link>
                     <Link href="#">
-                        <a className="hover:text-lightYellow">Resumé</a>
+                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Resumé</a>
+                    </Link>
+                </div>
+
+                {/* Hamburger Menu */}
+                <button
+                    id="menu-btn"
+                    type="button"
+                    className={`block hamburger ${open} md:hidden focus:outline-none`}
+                    onClick={handleIsClick}>
+                    <span class="hamburger-top"></span>
+                    <span class="hamburger-middle"></span>
+                    <span class="hamburger-bottom"></span>
+                </button>
+            </div>
+
+            {/* Mobile Menu */}
+            <div class="md:hidden">
+                <div
+                    id="menu"
+                    class={`absolute ${visibility} flex-col items-center self-end py-8 mt-5 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
+                    <Link href="#">
+                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
+                            Home
+                        </a>
+                    </Link>
+                    <Link href="#">
+                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
+                            About
+                        </a>
+                    </Link>
+                    <Link href="#">
+                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
+                            Projects
+                        </a>
+                    </Link>
+                    <Link href="#">
+                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
+                            Contact
+                        </a>
+                    </Link>
+                    <Link href="#">
+                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
+                            Resumé
+                        </a>
                     </Link>
                 </div>
             </div>
