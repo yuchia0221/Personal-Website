bd630835703705b0586e179a2586568a158fd6e2
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-06-04 22:08:17 +0800

    Update Navbar Style & Add Transition Animation

--- portfolio/pages/components/Navbar.js
+++ portfolio/pages/components/Navbar.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 function Navbar() {
     const [open, setOpen] = useState("");
-    const [visibility, setVisibility] = useState("hidden");
+    const [visibility, setVisibility] = useState(false);
     const [isClick, setIsClick] = useState(false);
     const handleIsClick = () => {
         setIsClick((isClick) => !isClick);
@@ -12,96 +13,125 @@ function Navbar() {
 
     useEffect(() => {
         if (isClick) {
-            setVisibility("flex");
+            setVisibility(true);
             setOpen("open");
         } else {
-            setVisibility("hidden");
+            setVisibility(false);
             setOpen("");
         }
     }, [isClick]);
 
     return (
-        // Navigation Bar
-        <nav className="relative w-screen p-6">
-            {/* Desktop Menu */}
-            <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <Link href="/">
-                        <a>
-                            <Image src="/logo.svg" alt="Logo" height="60vh" width="60vw" />
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a className="m-4 text-2xl text-bold font-bold lg:text-4xl">Aaron Chen</a>
-                    </Link>
+        <div>
+            <nav className=" shadow-sm fixed w-full z-10">
+                <div className="flex items-center h-20 w-full">
+                    <div className="flex w-full mx-20 items-center justify-between">
+                        {/* Left Navbar: Icon & Name */}
+                        <div className="flex  items-center justify-center space-x-3 flex-shrink-0">
+                            <Link href="/">
+                                <a>
+                                    <Image src="/logo.svg" alt="Logo" height="35vh" width="35vw" />
+                                </a>
+                            </Link>
+
+                            <Link href="/">
+                                <a>
+                                    <h1 className=" font-bold text-xl cursor-pointer">
+                                        Aaron<span className="text-lightBlue">Chen</span>
+                                    </h1>
+                                </a>
+                            </Link>
+                        </div>
+
+                        {/* Right Navbar: Links */}
+                        <div className="hidden md:block">
+                            <div className="ml-10 flex items-baseline space-x-4">
+                                <Link href="/#">
+                                    <a className="cursor-pointer text-blue-600 font-semibold px-3 py-2 text-md hover:font-black">
+                                        Home
+                                    </a>
+                                </Link>
+                                <Link href="/#">
+                                    <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                        About
+                                    </a>
+                                </Link>
+                                <Link href="/#">
+                                    <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                        Projects
+                                    </a>
+                                </Link>
+                                <Link href="/#">
+                                    <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                        Contact
+                                    </a>
+                                </Link>
+                                <Link href="/#">
+                                    <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                        Resumé
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Hamburger Menu */}
+                    <div className="mr-10 flex">
+                        <button
+                            id="menu-btn"
+                            type="button"
+                            title="hamburger menu button"
+                            role="none"
+                            className={`block hamburger ${open} md:hidden focus:outline-none`}
+                            onClick={handleIsClick}>
+                            <span className="hamburger-top"></span>
+                            <span className="hamburger-middle"></span>
+                            <span className="hamburger-bottom"></span>
+                        </button>
+                    </div>
                 </div>
 
-                {/* Menu Items */}
-                <div className="hidden items-center md:flex">
-                    <Link href="#">
-                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Home</a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">About</a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Projects</a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Contact</a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hidden desk-nav px-6 hover:text-lightYellow md:flex">Resumé</a>
-                    </Link>
-                </div>
-
-                {/* Hamburger Menu */}
-                <button
-                    id="menu-btn"
-                    type="button"
-                    title="hamburger menu button"
-                    role="none"
-                    className={`block hamburger ${open} md:hidden focus:outline-none`}
-                    onClick={handleIsClick}>
-                    <span className="hamburger-top"></span>
-                    <span className="hamburger-middle"></span>
-                    <span className="hamburger-bottom"></span>
-                </button>
-            </div>
-
-            {/* Mobile Menu */}
-            <div className="md:hidden">
-                <div
-                    id="menu"
-                    className={`absolute ${visibility} flex-col items-center self-end py-8 mt-5 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}>
-                    <Link href="#">
-                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
-                            Home
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
-                            About
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
-                            Projects
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
-                            Contact
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a className="hover:text-lightYellow" onClick={handleIsClick}>
-                            Resumé
-                        </a>
-                    </Link>
-                </div>
-            </div>
-        </nav>
+                {/* Mobile Menu */}
+                <Transition
+                    show={visibility}
+                    enter="transition ease-out duration-100 transform"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="transition ease-in duration-75 transform"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95">
+                    <div className="md:hidden" id="mobile-menu">
+                        <div className="text-center px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            <Link href="/#">
+                                <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                                    Home
+                                </a>
+                            </Link>
+                            <Link href="/#">
+                                <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                                    About
+                                </a>
+                            </Link>
+                            <Link href="/#">
+                                <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                                    Projects
+                                </a>
+                            </Link>
+                            <Link href="/#">
+                                <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                                    Contact
+                                </a>
+                            </Link>
+                            <Link href="/#">
+                                <a className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                                    Resumé
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </Transition>
+            </nav>
+        </div>
     );
 }
 
