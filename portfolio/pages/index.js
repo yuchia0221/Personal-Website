207b3443f024a8bab9cbceedf99e6fe479d6e65d
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-08-26 20:38:17 -0500

    Remove Paper.js

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import Canvas from "../components/Canvas";
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar />
+            <Canvas />
         </>
     );
 }
