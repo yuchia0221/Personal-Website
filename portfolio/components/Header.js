38f2e5ccafeaac00128ffc36320dee4f715e166c
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-08-29 22:53:50 -0500

    Add Meta Data in Header

--- portfolio/components/Header.js
+++ portfolio/components/Header.js
@@ -5,6 +5,9 @@ function Header({ title }) {
     return (
         <>
             <Head>
+                <meta charSet="UTF-8" />
+                <meta name="author" content="Yu-Chia Chen" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
