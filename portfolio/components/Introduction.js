a8d63d6803c835eb0fc6aa44072d93ce07c8617e
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-09-06 19:39:55 -0500

    Format CSS

--- portfolio/components/Introduction.js
+++ portfolio/components/Introduction.js
@@ -10,7 +10,7 @@ function Introduction() {
             <TypeWriter typingWords={typingWords} textColor="text-sky-700" />
             <h1 className="font-bold md:text-2xl">I love coding.</h1>
             <Link href="/about" passHref>
-                <a className="rounded-md bg-sky-700 py-2 px-4 font-bold text-white shadow-md md:shadow-lg shadow-sky-700/40 hover:bg-sky-800">
+                <a className="rounded-md bg-sky-700 py-2 px-4 font-bold text-white shadow-md shadow-sky-700/40 hover:bg-sky-800 md:shadow-lg">
                     Show me more
                 </a>
             </Link>
