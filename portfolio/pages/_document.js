132043a79810217e6ed9f1bbed92218a1ccfd2ec
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-09-07 23:55:05 -0500

    Remove an Useless Meta Tag

--- portfolio/pages/_document.js
+++ portfolio/pages/_document.js
@@ -6,7 +6,6 @@ export default function Document() {
             <Head>
                 <meta charSet="UTF-8" />
                 <meta name="author" content="Yu-Chia Chen" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="icon" href="/images/favicon.ico" />
             </Head>
             <body>
