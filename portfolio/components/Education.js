e3355802df8ececd266bd7808a3d263f5b8fbcb7
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-12-16 14:26:01 +0800

    Update Layout of Education Section

--- portfolio/components/Education.js
+++ portfolio/components/Education.js
@@ -20,7 +20,7 @@ const Education = () => {
                 <div className="relative col-span-1 col-start-2 row-span-2 h-12 w-12 justify-self-center lg:h-20 lg:w-20">
                     <Image src="/images/ntu-logo.png" alt="Rice Logo" layout="fill" />
                 </div>
-                <div className="col-span-4 row-span-2 space-y-1 text-sm lg:text-lg">
+                <div className="col-span-6 row-span-2 space-y-1 text-sm lg:text-lg">
                     <div>B.A. in Economics @ National Taiwan University</div>
                     <div className="text-gray-500">Sept. 2017 - June 2021</div>
                     <div>Computer Science Courses GPA: 3.94/4.3</div>
