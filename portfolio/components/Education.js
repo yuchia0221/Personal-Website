0507de67fdab1892c6d5c88f4c821f2d3f2febf3
Yu-Chia Chen <yuchiachen99@gmail.com>
2023-01-04 01:43:47 +0800

    Update Education Section Content

--- portfolio/components/Education.js
+++ portfolio/components/Education.js
@@ -13,9 +13,9 @@ const Education = () => {
                 <div className="col-span-6 row-span-2 space-y-1 text-sm lg:text-lg">
                     <div>M.S. in Computer Science @ Rice University</div>
                     <div className="text-gray-500">Aug. 2022 - Present | Specializes in Systems and Security</div>
-                    <div>
-                        2022 Fall: Web Development, Parallel Computing, Algorithms, Computer Architecture
-                    </div>
+                    <div>GPA: 4.0 / 4.0</div>
+                    <div>2023 Spring: Cloud Computing, Mutlicore Computing, High-Performance Architecture</div>
+                    <div>2022 Fall: Web Development, Parallel Computing, Algorithms, Computer Architecture</div>
                 </div>
                 <div className="relative col-span-1 col-start-2 row-span-2 h-12 w-12 justify-self-center lg:h-20 lg:w-20">
                     <Image src="/images/ntu-logo.png" alt="Rice Logo" layout="fill" />
@@ -23,7 +23,7 @@ const Education = () => {
                 <div className="col-span-6 row-span-2 space-y-1 text-sm lg:text-lg">
                     <div>B.A. in Economics @ National Taiwan University</div>
                     <div className="text-gray-500">Sept. 2017 - June 2021</div>
-                    <div>Computer Science Courses GPA: 3.94/4.3</div>
+                    <div>Computer Science Courses GPA: 3.94 / 4.3</div>
                 </div>
             </div>
         </div>
